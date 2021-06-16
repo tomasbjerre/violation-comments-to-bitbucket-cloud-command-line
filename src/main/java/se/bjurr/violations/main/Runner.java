ebde8e920a8ad236d39bfc96cd8e811d1ee01922
Tomas Bjerre <tomas.bjerre85@gmail.com>
2021-06-16 18:45:21 +0200

    feat: MSBuild parser

--- src/main/java/se/bjurr/violations/main/Runner.java
+++ src/main/java/se/bjurr/violations/main/Runner.java
@@ -51,8 +51,7 @@ public class Runner {
   public void main(final String args[]) throws Exception {
     final Argument<?> helpArgument = helpArgument("-h", "--help");
     final String parsersString =
-        Arrays.asList(Parser.values())
-            .stream()
+        Arrays.asList(Parser.values()).stream()
             .map((it) -> it.toString())
             .collect(Collectors.joining(", "));
     final Argument<List<List<String>>> violationsArg =
@@ -163,8 +162,7 @@ public class Runner {
       if (this.showDebugInfo) {
         System.out.println(
             "Given parameters:\n"
-                + Arrays.asList(args)
-                    .stream()
+                + Arrays.asList(args).stream()
                     .map((it) -> it.toString())
                     .collect(Collectors.joining(", "))
                 + "\n\nParsed parameters:\n"
